507303fccd84f219bee7a3d65d861d32cf176b23
Maria-lin <95876186+Maria-lin@users.noreply.github.com>
2022-05-06 17:29:19 +0100

    Add files via upload

---
+++ lib/data/data.dart
@@ -0,0 +1,18 @@
+List<Map<String, String>> splashData = [
+  {
+    "head":"Bienvenue dans PARKME",
+    "text": "propose des emplacement de garage  \n facilment loués près de  \n votre destination !",
+    "image": "assets/mycar.png"
+  },
+  {
+    "head":"paiment en ligne ",
+    "text": "paiment sans soucis .il suffit d'ajouter \n votre carte de credit et de payer   ",
+    "image": "assets/pfe.png"
+  },
+  {
+    "head":"Réserver Votre place ",
+    "text": "réserver et louer facilement votre place  \n de parking pour de l'argent  \n supplémentaire.",
+    "image": "assets/résever.png"
+  },
+
+];
